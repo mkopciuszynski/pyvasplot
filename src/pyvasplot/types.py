0bfeda39d546f61604e65dca515672b65a604370
Marek Kopciuszynski <mkopciuszynski@gmail.com>
2026-09-11 08:51:44 +0200

    ♻ Refactor CalculationType

--- src/pyvasplot/types.py
+++ src/pyvasplot/types.py
@@ -5,25 +5,25 @@ class CalculationType(StrEnum):
     """Types of VASP calculations supported by pyvasplot."""
 
     BS = "BS"
-    BS_PATH = "BS_PATH"
+    BS_KPATH = "BS_KPATH"
 
     HSE06 = "HSE06"
-    HSE06_PATH = "HSE06_PATH"
+    HSE06_KPATH = "HSE06_KPATH"
 
-    SO = "SO"
-    SO_PATH = "SO_PATH"
+    SO_STATIC = "SO_STATIC"
+    SO_STATIC_KPATH = "SO_STATIC_KPATH"
 
     KXKY = "KXKY"
-    SO_KXKY = "SO_KXKY"
+    SO_STATIC_KXKY = "SO_STATIC_KXKY"
 
 
     @property
     def is_path(self) -> bool:
         """Whether this calculation contains a k-path."""
         return self in {
-            CalculationType.BS_PATH,
-            CalculationType.HSE06_PATH,
-            CalculationType.SO_PATH,
+            CalculationType.BS_KPATH,
+            CalculationType.HSE06_KPATH,
+            CalculationType.SO_STATIC_KPATH,
         }
 
     @property
@@ -36,14 +36,14 @@ class CalculationType(StrEnum):
         """Whether this is an HSE calculation."""
         return self in {
             CalculationType.HSE06,
-            CalculationType.HSE06_PATH,
+            CalculationType.HSE06_KPATH,
         }
 
     @property
     def is_so(self) -> bool:
         """Whether this calculation includes spin-orbit coupling."""
         return self in {
-            CalculationType.SO,
-            CalculationType.SO_PATH,
-            CalculationType.SO_KXKY,
+            CalculationType.SO_STATIC,
+            CalculationType.SO_STATIC_PATH,
+            CalculationType.SO_STATIC_KXKY,
         }
